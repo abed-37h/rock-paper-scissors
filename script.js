e617b6944cf08342d337228d8671a1674651291c
abed-37h <79279228+abed-37h@users.noreply.github.com>
2024-01-27 18:56:50 +0200

    Take input from user interface
    Major changes:
    - Create a list of three buttons, each one represents a corresponding
    choice
    - Add an event listener for each choice button, to respond to the click
    event
    - Output is still being displayed in console
    
    Consequences:
    - Delete `playRound` function
    - Rename `playRoundHelper` to 'playRound`
    - Omit checking for `Error: invalid choice`, as it is now guaranteed to
    be valid

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
-function playRoundHelper(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     let choices = ['rock', 'paper', 'scissors'];
 
     playerSelection = playerSelection.toLowerCase();
@@ -14,9 +14,6 @@ function playRoundHelper(playerSelection, computerSelection) {
     let p = choices.indexOf(playerSelection);
     let c = choices.indexOf(computerSelection);
 
-    if (p === -1) return `Error: "${playerSelection}" is not a valid choice!
-    Available choices are ["rock", "paper", "scissors"]`;
-
     if (p === c) return `Tie! Re-play this round`;
     
     if ((p + 1) % 3 === c) return `You Lose! ${computerSelection} beats ${playerSelection}`;
@@ -24,44 +21,27 @@ function playRoundHelper(playerSelection, computerSelection) {
     return `You Win! ${playerSelection} beats ${computerSelection}`;
 }
 
-function playRound() {
-    let result;
-    do {
-        let playerSelection = prompt('Type your choice: ["rock", "paper", "scissors"]\n*Cancel or Esc to end the game*');
-        let computerSelection = getComputerChoice();
-        console.clear();
-
-        if (!playerSelection) return null;
-
-        result = playRoundHelper(playerSelection, computerSelection);
-        console.log(result);
-    } while (result.startsWith('Tie') || result.startsWith('Error'));
-
-    return result.startsWith('You Win');
-}
-
 function game() {
-    console.clear();
-    
     let playerScore = 0;
     let computerScore = 0;
-
-    let playerWin = null;
-    while ((playerScore + computerScore) < 5) {
-        playerWin = playRound();
-        if (playerWin == null) break;
-
-        playerWin ? playerScore++ : computerScore++;
-
-        console.log('Your Score: ' + playerScore + '\t' + ' Computer\'s Score: ' + computerScore);
-    }
-
-    if (playerWin !== null)
-        console.log((playerScore > computerScore) ? 'Hooray! You Win :D' : 'Good luck next time :(');
-
-    if (confirm('Do you want to restart the game?'))
-        setTimeout(game, 2000);
+    
+    const choices = document.querySelectorAll('.choice button');
+    choices.forEach((choice) => {
+        choice.addEventListener('click', (event) => {
+            console.clear();
+    
+            const computerSelection = getComputerChoice();
+            const playerSelection = choice.id;
+            const result = playRound(playerSelection, computerSelection);
+    
+            if (result.startsWith('You Win')) playerScore++;
+            else if (result.startsWith('You Lose')) computerScore++;
+            
+            console.log(result);
+            console.log(`Your score: ${playerScore}`);
+            console.log(`Computer score: ${computerScore}`);
+        });
+    });
 }
 
-if (confirm('Are you ready?\n*Open the console to get ready*'))
-    setTimeout(game, 2000);
+game()
